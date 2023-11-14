9ed45144a45b76dd36f27052893fb3505dc3f709
Maximilian Aoki <135271252+maximilian-aoki@users.noreply.github.com>
2023-11-14 17:12:00 -0500

    Add Menu generation and UI

--- src/assets/menu.js
+++ src/assets/menu.js
@@ -4,7 +4,54 @@ export function getMenu() {
 
   menu.innerHTML = 
     `
-    
+    <div class="product one">
+      <div class="product-image"></div>
+      <div class="product-details">
+        <p>Cyber Chocolate</p>
+        <p>$8</p>
+        <p>"Delta-driven dark chocolate for decoding deliciousness"</p>
+      </div>
+    </div>
+    <div class="product two">
+      <div class="product-image"></div>
+      <div class="product-details">
+        <p>Smart Strawberry</p>
+        <p>$8</p>
+        <p>"Berry intelligence at it's ~berry~ best ;D"</p>
+      </div>
+    </div>
+    <div class="product three">
+      <div class="product-image"></div>
+      <div class="product-details">
+        <p>Quantum Crunch</p>
+        <p>$12</p>
+        <p>"Crunchy ~bits~ in a delicious quantum entanglement"</p>
+      </div>
+    </div>
+    <div class="product four">
+      <div class="product-image"></div>
+      <div class="product-details">
+        <p>Binary Swirl</p>
+        <p>$7</p>
+        <p>"0s and 1s of sweet, sweet swirly satisfaction"</p>
+      </div>
+    </div>
+    <div class="product five">
+      <div class="product-image"></div>
+      <div class="product-details">
+        <p>Circuitry Sorbet</p>
+        <p>$9</p>
+        <p>"Zesty citrus with a spark of tech.. in a popsicle!"</p>
+      </div>
+    </div>
+    <div class="product six">
+      <div class="product-image"></div>
+      <div class="product-details">
+        <p>VST Velvet</p>
+        <p>$6</p>
+        <p>"A silky smooth simulation of chocolate flavour"</p>
+      </div>
+    </div>
     `
   ;
 
