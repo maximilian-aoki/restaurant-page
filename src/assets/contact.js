9bceb620a31058a29435f9fdcf86476af36972aa
Maximilian Aoki <135271252+maximilian-aoki@users.noreply.github.com>
2023-11-14 18:15:57 -0500

    Add contact generation and UI

--- src/assets/contact.js
+++ src/assets/contact.js
@@ -4,7 +4,41 @@ export function getContact() {
 
   contact.innerHTML = 
     `
-    
+    <div class="location-image"></div>
+    <div class="other-details">
+      <div class="location-info flex-column">
+        <h3>Location</h3>
+        <p>151 Gold Mine Road</p>
+        <p>San Francisco, California</p>
+        <p>M6S 4R5</p>
+      </div>
+      <div class="contact-info flex-column">
+        <h3>Contact</h3>
+        <p>info@roboticecream.com</p>
+        <p>(888) 531-5455</p>
+      </div>
+      <div class="hours flex-column">
+        <h3>Hours</h3>
+        <div class="hours-separation">
+          <div class="days">
+            <p>Monday</p>
+            <p>Tuesday</p>
+            <p>Wednesday</p>
+            <p>Thursday</p>
+            <p>Friday</p>
+            <p>Weekend</p>
+          </div>
+          <div class="times">
+            <p>11AM - 8PM</p>
+            <p>11AM - 8PM</p>
+            <p>11AM - 8PM</p>
+            <p>11AM - 10PM</p>
+            <p>11AM - 10PM</p>
+            <p>Closed</p>
+          </div>
+        </div>
+      </div>
+    </div>
     `
   ;
 
